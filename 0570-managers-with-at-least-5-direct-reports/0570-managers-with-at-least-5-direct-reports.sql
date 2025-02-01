93a4a344db1023fec75dc1b085df17c0cb4a4c69
Josephine Adongo <adongojosephine13@gmail.com>
2025-02-01 22:50:44 +0300

    Time: 661 ms (37.04%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,11 +1,11 @@
 # Write your MySQL query statement below
 with manager as(
-  select e1.id, e1.name, e1.managerId,
-  count(e1.id) as direct_reports
-  from employee e1
-  join employee e2 on e1.id = e2.managerId
-  where e2.managerId is not null
-  group by e1.id, e1.name
+  select e.id, e.name, e.managerId,
+  count(e.id) as direct_reports
+  from employee e
+  join employee m on e.id = m.managerId
+  where m.managerId is not null
+  group by e.id, e.name
 )
 select name
 from manager
